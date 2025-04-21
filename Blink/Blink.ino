03cd34ebd9f31e92c0f2713efca150c6ddf7ac6e
Hunt <konnerhunt81@gmail.com>
2025-04-21 14:04:55 -0400

    Updated Code w/ greenLED = 12;

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -21,17 +21,17 @@
 
   https://docs.arduino.cc/built-in-examples/basics/Blink/
 */
-
+int greenLED = 12;
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin 12 as an output.
-  pinMode(12, OUTPUT);
+    pinMode(greenLED, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(12, HIGH);  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(greenLED, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(3000);                      // wait for a second
-  digitalWrite(12, LOW);   // turn the LED off by making the voltage LOW
+  digitalWrite(greenLED, LOW);   // turn the LED off by making the voltage LOW
   delay(500);                      // wait for a second
 }
